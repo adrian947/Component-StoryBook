a0aad3c748feffd00052594cd5acea8da284d175
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-04-10 17:16:39 -0300

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,46 @@
+import "./mylabel.css";
+import { CustomFontColor } from "../stories/components/MyLabel.stories";
+
+export interface Props {
+  /**
+   * Is this name for label
+   */
+  label: string;
+  /**
+   * Is this size for label
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Text transform Uppercase
+   */
+  allCaps: boolean;
+  /**
+   * Basic color label
+   */
+
+  color: "primary" | "secondary" | "tertiary";
+  /**
+   * Custom Color label
+   */
+  customColor?: string;
+}
+
+/**
+ * Label component for user interaction
+ */
+export const MyLabel = ({
+  label = "no Label",
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  customColor,
+}: Props) => {
+  return (
+    <span
+      className={`${size} ${allCaps && "allCaps"} label text-${color}`}
+      style={{ color: customColor}}
+    >
+      {label}
+    </span>
+  );
+};
